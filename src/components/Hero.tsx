154cfa443b3ba5a4129814440a5d5ae5f84c516a
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-08-15 18:40:19 +0530

    HeroSection: Add lottie file

---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Lottie from 'lottie-react'
+import React, { FC } from 'react'
+
+interface Props{
+
+}
+
+export const Hero:FC<Props> = () => {
+  return (
+    <div className='flex justify-between'>
+      <div className='flex-grow'>
+        text
+      </div>
+      <div className='w-1/2'>
+        <Lottie
+          animationData={require('public/heroLottie.json')}
+          style={{ width: '100%', height: '100%' }}
+        />
+        </div> 
+    </div>
+  )
+}
