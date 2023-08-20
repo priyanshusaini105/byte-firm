9d7ab09fee7eef44e63392eaa46aeb7f0046024a
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-08-20 16:07:32 +0530

    Create CTO Button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 import Lottie from "lottie-react";
 import Link from "next/link";
 import React, { FC } from "react";
@@ -7,16 +8,16 @@ interface Props {}
 
 export const Hero: FC<Props> = () => {
   return (
-    <section className="flex justify-between w-full flex-col md:flex-row">
+    <section className="flex justify-between w-full flex-col md:flex-row md:p-12 p-8">
       <div className="w-full p-4 text-white relative">
-        <h1 className="text-4xl md:text-5xl block font-bold text-primary-800">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl block font-bold text-primary-700">
           Transforming <span className="text-accent-400">Ideas</span>
         </h1>
-        <span className="text-lg text-primary-700">into </span>
-        <h3 className="text-4xl md:text-5xl block relative md:left-0 font-bold text-accent-400">
+        <span className="text-lg md:text-xl text-primary-700">into </span>
+        <h3 className="text-4xl md:text-5xl lg:text-6xl block relative md:left-0 font-bold text-accent-400">
           Reality.
         </h3>
-        <p className="text-white relative text-md my-8 text-primary-600">
+        <p className="relative text-md my-8 text-primary-600 md:pr-12 text-justify">
           <span className="text-lg font-bold text-800">
             "Your Dream Apps and Websites, Created by Experts."
           </span>
@@ -24,13 +25,13 @@ export const Hero: FC<Props> = () => {
           React Native developers, turning your concepts into stunning,
           functional apps and websites.
         </p>
-        <Link
-          href=""
-          className="text-gray-50 bg-accent-400 p-3 rounded-lg m-4 self-center font-bold"
-        >
+        {/* cto button */}
+
+        <Link href="" className="cto-btn">
           Request a Quote
         </Link>
       </div>
+
       <div className="md:w-full">
         <Lottie
           animationData={require("public/heroLottie.json")}
