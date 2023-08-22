56a7597e370e9df5fae9e1ff42eb4f58bb90fde6
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-08-22 16:40:47 +0530

    Add fonts to hero and body

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,15 +11,15 @@ export const Hero: FC<Props> = () => {
     <section className="flex justify-between w-full flex-col md:flex-row md:p-12 p-8 mt-16">
       <Fade top>
         <div className="w-full p-4 text-white relative">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl block font-bold text-primary-700">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl block font-bold text-primary-700 font-display">
             Transforming <span className="text-accent-400">Ideas</span>
           </h1>
           <span className="text-lg md:text-xl text-primary-700">into </span>
-          <h3 className="text-4xl md:text-5xl lg:text-6xl block relative md:left-0 font-bold text-accent-400">
+          <h3 className="text-4xl md:text-5xl lg:text-6xl block relative md:left-0 font-bold text-accent-400 font-display">
             Reality.
           </h3>
           <p className="relative text-md my-8 text-primary-600 md:pr-12 text-justify">
-            <span className="text-lg font-bold text-800">
+            <span className="text-lg font-bold text-800  font-display">
               "Your Dream Apps and Websites, Created by Experts."
             </span>
             <br /> Welcome to Byte Firm. We're your dedicated team of React and
