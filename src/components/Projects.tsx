81aab7e526222713fd056445ced2a560c57685de
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-09-01 22:56:29 +0530

    Create Descriptions of the card

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,23 +3,23 @@
 import React from "react";
 import Image from "next/image";
 
-type Project={
-  id:string,
-  title:string,
-  img:string,
-  desc:string
-  techs:string[],
-  url?:string
-}
+type Project = {
+  id: string;
+  title: string;
+  img: string;
+  desc: string;
+  techs: string[];
+  url?: string;
+};
 
-const projects:Project[] = [
+const projects: Project[] = [
   {
     id: "1",
     title: "Wordcraft",
-    img:'wordcraft.png',
-    desc:'Wordcraft is a blog website where anyone can start posting their blogs by creating their own account.',
-    techs:['Next Js','Firebase','React Js','Tailwind CSS'],
-    url:'https://wordcraft-blog.vercel.app/'
+    img: "wordcraft.png",
+    desc: "Wordcraft is a blog website where anyone can start posting their blogs by creating their own account.",
+    techs: ["Next Js", "Firebase", "React Js", "Tailwind CSS"],
+    url: "https://wordcraft-blog.vercel.app/",
   },
 ];
 
@@ -41,18 +41,46 @@ export function Projects() {
         <div className="w-9/10 mx-auto rounded-lg bg-accent-500 h-4 shadow-2xl shadow-blue-500/20 "></div>
         <div className="flex flex-wrap gap-8 justify-center">
           {projects.map((project) => (
-            <div key={project.id} className="relative w-56 h-[300px] bg-[whitesmoke] shadow-xl flex items-center justify-center text-black rounded-[10px] book bg">
-              
+            <div
+              key={project.id}
+              className="relative w-56 h-[300px] bg-[whitesmoke] shadow-xl flex items-center justify-center text-black rounded-[10px] book bg"
+            >
+              <div className="absolute top-0 left-0 w-9/10 h-full bg-[whitesmoke] bg-opacity-80 flex flex-col items-center justify-center gap-4 p-4 rounded-[10px] transition-all duration-500 opacity-0 hover:opacity-100 ml-4">
+                <p className="text-xl font-bold font-display">
+                  {project.title}
+                </p>
+                <p className="text-sm">{project.desc}</p>
+                <div className="flex flex-wrap gap-2">
+                  {project.techs.map((tech) => (
+                    <div
+                      key={tech}
+                      className="bg-accent-500 text-black px-2 py-1 rounded-md text-sm"
+                    >
+                      {tech}
+                    </div>
+                  ))}
+                </div>
+                {project.url && (
+                  <a
+                    href={project.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-primary-500 text-white px-2 py-1 rounded-md text-sm"
+                  >
+                    Visit
+                  </a>
+                )}
+              </div>
 
               <div className="cover absolute bg-[lightgray] w-full h-full cursor-pointer transition-all duration-500 shadow-xl flex items-center justify-center rounded-[10px] top-0 flex flex-col">
                 <p>{project.title}</p>
                 <Image
-                 src={'/images/projects/'+project.img}
-                 alt=''
-                 width={280}
-                 height={480}
-                 className='w-full'
-                 />
+                  src={"/images/projects/" + project.img}
+                  alt=""
+                  width={280}
+                  height={480}
+                  className="w-full"
+                />
               </div>
             </div>
           ))}
